6de96e61369a78e630b2d8fa85639ced850842a7
Anaicruz7 <alisoncruz5565@gmail.com>
2025-02-26 13:11:11 -0600

    mi primer commit

---
+++ ejemplo2_19795333/cookies.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ejemplo2_19795333
+{
+	public partial class cookies : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+        protected void btnConfirm_Click(object sender, EventArgs e)
+        {
+            Response.Redirect
+                (
+            "Cokkie2ConfirmProduct.aspx" + "?ddlCategory=" + ddlCategory.SelectedValue + "&ddlSupplier=" + ddlSupplier.SelectedValue + "&txtProducto=" + txtProduct.Text + "&txtDescripcion=" + txtDescripcion.Text + "&txtImagen=" + txtImagen.Text + "&decprice=" + txtPrice.Text + "&byNumberInStock=" + txtNumberInStock.Text + "&byNumberOnOrder=" + txtNumberOnOrder.Text + "&byReorderLevel=" + txtNumberOnOrder.Text);
+        }
+    }
+}
